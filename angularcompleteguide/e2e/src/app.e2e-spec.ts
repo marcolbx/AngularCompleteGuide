9a3507eafe641358b8ee49634596a7c3e4568417
marcolbx <marcolbx@hotmail.com>
2020-07-10 19:56:52 -0400

    Initial commit

--- angularcompleteguide/e2e/src/app.e2e-spec.ts
+++ angularcompleteguide/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('AngularCompleteGuide app is running!');
+    expect(page.getTitleText()).toEqual('angularcompleteguide app is running!');
   });
 
   afterEach(async () => {
--- angularcompleteguide/src/app/app.component.spec.ts
+++ angularcompleteguide/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularCompleteGuide'`, () => {
+  it(`should have as title 'angularcompleteguide'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularCompleteGuide');
+    expect(app.title).toEqual('angularcompleteguide');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AngularCompleteGuide app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angularcompleteguide app is running!');
   });
 });
